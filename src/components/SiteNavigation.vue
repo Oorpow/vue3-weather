dcdb44197b1e390ec60846432fbac6906ac605fc
Oorpow <309754513@qq.com>
2023-06-04 20:54:57 +0800

    feat: lesson4

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -11,14 +11,57 @@
 				<FontAwesomeIcon
 					icon="fa-solid fa-circle-info"
 					class="text-xl hover:text-weather-secondary cursor-pointer transition-all delay-100"
+					@click="modalActive = !modalActive"
 				/>
 				<FontAwesomeIcon
 					icon="fa-solid fa-plus"
 					class="text-xl hover:text-weather-secondary cursor-pointer transition-all delay-100"
 				/>
 			</div>
+			<!-- 模态框 -->
+			<BaseModal :modal-active="modalActive" @toggle-modal="toggleModal">
+				<div class="text-black">
+					<h1 class="text-2xl mb-1">About:</h1>
+					<p class="mb-4">
+						The Local Weather allows you to track the current and
+						future weather of cities of your choosing.
+					</p>
+					<h2 class="text-2xl">How it works:</h2>
+					<ol class="list-decimal list-inside mb-4">
+						<li>
+							Search for your city by entering the name into the
+							search bar.
+						</li>
+						<li>
+							Select a city within the results, this will take you
+							to the current weather for your selection.
+						</li>
+						<li>
+							Track the city by clicking on the "+" icon in the
+							top right. This will save the city to view at a
+							later time on the home page.
+						</li>
+					</ol>
+
+					<h2 class="text-2xl">Removing a city</h2>
+					<p>
+						If you no longer wish to track a city, simply select the
+						city within the home page. At the bottom of the page,
+						there will be am option to delete the city.
+					</p>
+				</div>
+			</BaseModal>
 		</nav>
 	</header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import BaseModal from './BaseModal.vue'
+
+const modalActive = ref(false)
+
+const toggleModal = () => {
+    modalActive.value = !modalActive.value
+}
+</script>
