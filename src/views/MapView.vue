4e0875208fe21be4cc58479ceb2865179eeac16b
Oorpow <309754513@qq.com>
2023-06-23 00:12:13 +0800

    feat: 实现搜索提示和地图点标注

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,60 +1,83 @@
 <template>
 	<KeepAlive>
-		<div id="container" class="w-full p0 m0 h-screen relative"></div>
+		<div id="container" class="w-full h-screen relative"></div>
 	</KeepAlive>
 	<div class="absolute top-[100px] left-[20px] w-[400px]">
-		<input
-			type="search"
-			class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-			v-model="searchLocation"
-			@input="searchPOI"
-		/>
-		<!-- list -->
-		<div class="bg-white px-2 rounded-b-lg">
-			<ul>
-				<li class="flex justify-between first:mt-0 py-2 border-b last:border-none border-gray-200 hover:bg-gray-50 cursor-pointer" v-for="item in 4">
-					<div>
-						<h4>1. 广州北站</h4>
-						<span class="text-sm text-gray-400 block mt-2">站前路1号</span>
-					</div>
-					<div class="w-[100px] h-[80px]">
-						<img class="w-full h-full object-cover" src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg" alt="">
-					</div>
-				</li>
-			</ul>
+		<!-- 搜索框 -->
+		<div id="myPageTop">
+			<input
+				id="tipinput"
+				type="search"
+				class="input-search"
+				placeholder="请输入关键字"
+			/>
 		</div>
+		<!-- 搜索结果面板显示 -->
+		<div id="panel" class="top-[100px] left-[50px] w-full h-[300px]"></div>
 	</div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref, shallowRef } from 'vue'
-import { useThrottleFn } from '@vueuse/core'
 import AMapLoader from '@amap/amap-jsapi-loader'
 
-const map = shallowRef(null)
-const searchLocation = ref('')
+const map: any = shallowRef(null)
+/** 存放placeSearch插件实例对象 */
+const placeSearch: any = ref(null)
 
-const searchPOI = useThrottleFn(() => {
-	console.log(searchLocation.value)
-}, 1000)
+/** 插件列表 */
+const mapPluginsList: string[] = ['AMap.PlaceSearch', 'AMap.AutoComplete']
 
+/** 搜索插件配置项 */
+const searchPluginOptions = {
+	pageSize: 5, // 单页显示结果条数
+	pageIndex: 1, // 页码
+	map: null, // 展现结果的地图实例
+	panel: 'panel', // 结果列表将在此容器中进行展示。
+	autoFitView: true, // 是否自动调整地图视野使绘制的 Marker点都处于视口的可见范围
+}
+
+/** 搜索提示插件配置项 */
+const autoCompletePluginOptions = {
+	input: 'tipinput',
+}
+
+/** 地图初始化 */
 const initMap = () => {
+	;(window as any)._AMapSecurityConfig = {
+		securityJsCode: import.meta.env.VITE_MAP_SECURITY_KEY,
+	}
+
 	AMapLoader.load({
 		key: import.meta.env.VITE_MAP_KEY, // 申请好的Web端开发者Key，首次调用 load 时必填
 		version: '2.0', // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-		plugins: [''], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
 	})
 		.then((AMap) => {
+			// 注册实例对象
 			map.value = new AMap.Map('container', {
-				//设置地图容器id
-				viewMode: '3D', //是否为3D地图模式
-				zoom: 5, //初始化地图级别
-				center: [105.602725, 37.076636], //初始化地图中心点位置
+				zoom: 13, //初始化地图级别
+			})
+			searchPluginOptions.map = map.value
+			// 挂载插件
+			AMap.plugin(mapPluginsList, () => {
+				const autoComplete = new AMap.AutoComplete(
+					autoCompletePluginOptions
+				)
+				placeSearch.value = new AMap.PlaceSearch(searchPluginOptions)
+				autoComplete.on('select', handleSelect)
 			})
 		})
 		.catch((e) => {
 			console.log(e)
 		})
+}
+
+/** 当用户选中搜索结果的项时触发 */
+const handleSelect = (e: any) => {
+	placeSearch.value.setCity(e.poi.adcode)
+	placeSearch.value.search(e.poi.name, (status, result) => {
+		// 自定义选点
+	})
 }
 
 onMounted(() => {
